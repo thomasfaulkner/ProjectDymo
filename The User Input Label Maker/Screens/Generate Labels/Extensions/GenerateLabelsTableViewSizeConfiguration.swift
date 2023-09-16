ed5493072444b048866b874a0aa962b53c727d5c
Thomas Faulkner <thomasfaulkner@ThomassCEMBP2.attlocal.net>
2023-09-15 20:18:11 -0400

    Filled out Generate Labels TVC size configuration

--- The User Input Label Maker/Screens/Generate Labels/Extensions/GenerateLabelsTableViewSizeConfiguration.swift
+++ The User Input Label Maker/Screens/Generate Labels/Extensions/GenerateLabelsTableViewSizeConfiguration.swift
@@ -6,3 +6,23 @@
 //
 
 import UIKit
+
+extension GenerateLabelsTableViewController {
+    // MARK: Configure Size
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        UITableView.automaticDimension
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForHeaderInSection section: Int) -> CGFloat {
+        return 53
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        UITableView.automaticDimension
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForFooterInSection section: Int) -> CGFloat {
+        return 53
+    }
+}
