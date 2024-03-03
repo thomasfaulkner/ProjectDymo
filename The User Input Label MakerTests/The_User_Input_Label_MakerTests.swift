5f4f0d6b64032c1143f87bc386f5813db6739c06
Thomas Faulkner <turniphop@gmail.com>
2024-03-03 14:41:15 -0500

    Added test_TextFieldCell_WhenAssignedText_StoresReadableText()

--- The User Input Label MakerTests/The_User_Input_Label_MakerTests.swift
+++ The User Input Label MakerTests/The_User_Input_Label_MakerTests.swift
@@ -18,19 +18,22 @@ final class The_User_Input_Label_MakerTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func test_TextFieldCell_WhenAssignedText_StoresReadableText() throws {
+        // Given
+        let sut = TextFieldTableViewCell()
+        
+        // When
+        sut.textField.text = "Sample text"
+        
+        // Then
+        XCTAssertEqual(sut.textField.text, "Sample text", "Text field text does not match text assigned initially.")
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
