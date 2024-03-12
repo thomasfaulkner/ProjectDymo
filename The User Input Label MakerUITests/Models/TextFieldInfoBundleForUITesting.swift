f3ed2b3f9158745e199903e8677661a4d2041dea
Thomas Faulkner <turniphop@gmail.com>
2024-03-11 20:19:14 -0400

    Expanded TextFieldInfoBundleForUITesting with optional properties for second and third text fields, for use in later integration testing

--- The User Input Label MakerUITests/Models/TextFieldInfoBundleForUITesting.swift
+++ The User Input Label MakerUITests/Models/TextFieldInfoBundleForUITesting.swift
@@ -10,16 +10,40 @@ import XCTest
 
 struct TextFieldInfoBundleForUITesting {
     var tableName: String
+    
     var fieldName: String
+    var secondFieldName: String?
+    var thirdFieldName: String?
     
     let app = XCUIApplication()
     
-    // Get correct text field via the cell's automation identifier
+    // Get specified text field via the cell's automation identifier
     var textField: XCUIElement {
         return app.tables[tableName].textFields[fieldName]
     }
     
+    var secondTextField: XCUIElement? {
+        guard let secondFieldName = secondFieldName else { return nil }
+        return app.tables[tableName].textFields[secondFieldName]
+    }
+    
+    var thirdTextField: XCUIElement? {
+        guard let thirdFieldName = thirdFieldName else { return nil }
+        return app.tables[tableName].textFields[thirdFieldName]
+    }
+    
+    // Get the contents of a specified text field
     var textFieldContents: String {
         return textField.value as! String
+    }
+    
+    var secondTextFieldContents: String? {
+        guard let secondTextField = secondTextField else { return nil }
+        return (secondTextField.value as! String)
+    }
+    
+    var thirdTextFieldContents: String? {
+        guard let thirdTextField = thirdTextField else { return nil }
+        return (thirdTextField.value as! String)
     }
 }
