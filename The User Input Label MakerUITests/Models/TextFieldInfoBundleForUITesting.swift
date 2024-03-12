51a984b3e9dc977f20dbccc5b57dd8b6378819d2
Thomas Faulkner <turniphop@gmail.com>
2024-03-12 19:56:54 -0400

    Made several properties in TextFieldInfoBundleForUITesting struct read-only

--- The User Input Label MakerUITests/Models/TextFieldInfoBundleForUITesting.swift
+++ The User Input Label MakerUITests/Models/TextFieldInfoBundleForUITesting.swift
@@ -19,31 +19,43 @@ struct TextFieldInfoBundleForUITesting {
     
     // Get specified text field via the cell's automation identifier
     var textField: XCUIElement {
-        return app.tables[tableName].textFields[fieldName]
+        get {
+            return app.tables[tableName].textFields[fieldName]
+        }
     }
     
     var secondTextField: XCUIElement? {
-        guard let secondFieldName = secondFieldName else { return nil }
-        return app.tables[tableName].textFields[secondFieldName]
+        get {
+            guard let secondFieldName = secondFieldName else { return nil }
+            return app.tables[tableName].textFields[secondFieldName]
+        }
     }
     
     var thirdTextField: XCUIElement? {
-        guard let thirdFieldName = thirdFieldName else { return nil }
-        return app.tables[tableName].textFields[thirdFieldName]
+        get {
+            guard let thirdFieldName = thirdFieldName else { return nil }
+            return app.tables[tableName].textFields[thirdFieldName]
+        }
     }
     
     // Get the contents of a specified text field
     var textFieldContents: String {
-        return textField.value as! String
+        get {
+            return textField.value as! String
+        }
     }
     
     var secondTextFieldContents: String? {
-        guard let secondTextField = secondTextField else { return nil }
-        return (secondTextField.value as! String)
+        get {
+            guard let secondTextField = secondTextField else { return nil }
+            return (secondTextField.value as! String)
+        }
     }
     
     var thirdTextFieldContents: String? {
-        guard let thirdTextField = thirdTextField else { return nil }
-        return (thirdTextField.value as! String)
+        get {
+            guard let thirdTextField = thirdTextField else { return nil }
+            return (thirdTextField.value as! String)
+        }
     }
 }
